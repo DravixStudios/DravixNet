59f04c84416f57e17c561ccb66d6aa5a5c13e909
PR3C14D0 <aarongarpre@gmail.com>
2025-08-27 15:57:05 +0200

    Application: AuthService finished
    May need some optimisations but done

--- backend/src/application/auth/AuthService.ts
+++ backend/src/application/auth/AuthService.ts
@@ -1,12 +1,16 @@
 import type { AuthUser } from "@/domain/auth/AuthUser";
 import type { IAuthRepository } from "@/domain/auth/IAuthRepository";
 import type { IHashProvider } from "@/domain/auth/IHashProvider";
+import type { ITokenProvider } from "@/domain/auth/ITokenProvider";
 import { EAuthErrorType, type AuthError } from "@/domain/auth/AuthError";
+import type { ISecretProvider } from "@/domain/ISecretProvider";
 
 export class AuthService {
     constructor(
         private authRepository: IAuthRepository,
-        private hashProvider: IHashProvider
+        private hashProvider: IHashProvider,
+        private tokenProvider: ITokenProvider,
+        private secretProvider: ISecretProvider
     ) {}
 
     async register(username: string, email: string, password: string): Promise<AuthUser | AuthError> {
@@ -30,5 +34,50 @@ export class AuthService {
         const savedUser: AuthUser = await this.authRepository.save(user);
 
         return savedUser;
+    }
+
+    async login(username: string, password: string): Promise<string | AuthError> {
+        const bUserExists = await this.authRepository.usernameExists(username);
+        if(!bUserExists) {
+            const err: AuthError = {
+                type: EAuthErrorType.UserNotFound,
+                errors: [
+                    "AuthError.UserNotFound"
+                ]
+            };
+
+            return err;
+        }
+
+        const user: AuthUser = await this.authRepository.findByUsername(username);
+
+        const bValidPassword: boolean = await this.hashProvider.compare(password, user.password);
+
+        if(!bValidPassword) {
+            const err: AuthError = {
+                type: EAuthErrorType.InvalidPassword,
+                errors: [
+                    "AuthError.InvalidPassword"
+                ]
+            };
+
+            return err;
+        }
+
+        const tokenSecret: string | null | undefined = this.secretProvider.tokenSecret;
+
+        if(!tokenSecret) {
+            const err: AuthError = {
+                type: EAuthErrorType.UndefinedError,
+                errors: [
+                    "AuthError.UndefinedError"
+                ]
+            };
+
+            return err;
+        }
+
+        const token: string = await this.tokenProvider.sign({username: user.username, steam_id: user.steam_id ? user.steam_id : 0}, tokenSecret);
+        return token;
     }
 }
--- backend/src/domain/auth/AuthError.ts
+++ backend/src/domain/auth/AuthError.ts
@@ -2,6 +2,7 @@ export enum EAuthErrorType {
     EmailExists = 1 << 0,
     UsernameExists = 1 << 1,
     UserNotFound = 1 << 2,
+    InvalidPassword = 1 << 3,
     UndefinedError = 0xFF
 }
 
