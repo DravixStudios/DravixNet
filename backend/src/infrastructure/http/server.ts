227d0e37987f5a316d2468f60ec935a6ffd3ffde
PR3C14D0 <aarongarpre@gmail.com>
2025-08-28 00:49:47 +0200

    Package.json: Fixed the dev script

--- backend/src/infrastructure/http/server.ts
+++ backend/src/infrastructure/http/server.ts
@@ -1,10 +1,16 @@
 import Express from 'express';
 import { EnvAdapter } from '@/infrastructure/EnvAdapter';
+
+import api from './routers/api.router';
+
 const app = Express();
+app.use(Express.json());
 
 const port: string = EnvAdapter.port;
 
 app.set('port', port)
+
+app.use('/api', api);
 
 app.listen(app.get('port'), () => {
     console.log("Listening on port", app.get('port'))
