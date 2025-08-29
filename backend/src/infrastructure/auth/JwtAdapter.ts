003b0e1454e64af5e2c609f824976d1730879c2b
PR3C14D0 <aarongarpre@gmail.com>
2025-08-29 03:23:06 +0200

    Infrastructure: The JWT adapter is now adapted to the new Token provider structure

--- backend/src/infrastructure/auth/JwtAdapter.ts
+++ backend/src/infrastructure/auth/JwtAdapter.ts
@@ -2,18 +2,26 @@ import type { ITokenData } from "@/domain/auth/ITokenData";
 import type { ITokenProvider } from "@/domain/auth/ITokenProvider";
 import jwt, { type JwtPayload, type VerifyErrors } from 'jsonwebtoken';
 
+interface JwtTokenData extends JwtPayload {
+    username: string;
+    steam_id: string | number;
+}
+
 export const JwtAdapter: ITokenProvider = {
     sign: async (payload: ITokenData, secret: string): Promise<string> => {
         const token: string = await jwt.sign(payload, secret);
         return token;
     },
     
-    verify: async (token: string, secret: string): Promise<boolean> => {
+    verify: async (token: string, secret: string): Promise<ITokenData | null> => {
         try {
-        await jwt.verify(token, secret);
-        return true;
-    } catch (e) {
-        return false;
-    }
+            const decoded: JwtTokenData = await jwt.verify(token, secret) as JwtTokenData;
+            return {
+                username: decoded.username,
+                steam_id: decoded.steam_id
+            };
+        } catch(e) {
+            return null;
+        }
     }
 }
