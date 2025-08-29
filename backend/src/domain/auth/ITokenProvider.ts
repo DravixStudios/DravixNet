e34479a416e1ae8395c9dd5de9ef7f1ec784ac94
PR3C14D0 <aarongarpre@gmail.com>
2025-08-29 03:22:46 +0200

    Token provider: The token adapter now returns the Token data

--- backend/src/domain/auth/ITokenProvider.ts
+++ backend/src/domain/auth/ITokenProvider.ts
@@ -2,5 +2,5 @@ import type { ITokenData } from "./ITokenData";
 
 export interface ITokenProvider {
     sign(payload: ITokenData, secret: string): Promise<string>;
-    verify(token: string, secret: string): Promise<boolean>;
+    verify(token: string, secret: string): Promise<ITokenData | null>;
 }
