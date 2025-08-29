a1e1633ecc0492bb1e7e3711ec6c903cf38f8a68
PR3C14D0 <aarongarpre@gmail.com>
2025-08-29 04:49:02 +0200

    HTTP server: added morgan

--- backend/src/infrastructure/http/server.ts
+++ backend/src/infrastructure/http/server.ts
@@ -2,9 +2,11 @@ import Express from 'express';
 import { EnvAdapter } from '@/infrastructure/EnvAdapter';
 
 import api from './routers/api.router';
+import morgan from 'morgan';
 
 const app = Express();
 app.use(Express.json());
+app.use(morgan('dev'));
 
 const port: string = EnvAdapter.port;
 
